673a010d67beb266e1a99f6d57c59a2476fd558e
Bodil <xxc3303@gmail.com>
2012-02-28 23:21:09 +0800

    updated let it work now.

--- src/com/gourmand/GourmandActivity.java
+++ src/com/gourmand/GourmandActivity.java
@@ -1,13 +1,13 @@
 package com.gourmand;
 
-import android.app.Activity;
+import com.phonegap.DroidGap;
 import android.os.Bundle;
 
-public class GourmandActivity extends Activity {
+public class GourmandActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/pages/index.html");
     }
 }
